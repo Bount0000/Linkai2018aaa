2a6e921abcebb65ff3ca3350bd5eb78e4c90c336
Bount0000 <31384977+Bount0000@users.noreply.github.com>
2018-01-22 16:58:14 +0800

    ces

--- app/src/main/java/com/lenovo/bount/linkai2018aaa/MainActivity.java
+++ app/src/main/java/com/lenovo/bount/linkai2018aaa/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String name="林凯";
-
+        //ceshi
     }
     }
